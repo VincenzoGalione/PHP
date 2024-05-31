da277dc33146da6768c92ace65bdf86af9205972
VincenzoGalione <165274170+VincenzoGalione@users.noreply.github.com>
2024-05-31 18:39:56 +0200

    sesto esercizio

--- php_06_Vincenzo_Galione.php
+++ php_06_Vincenzo_Galione.php
@@ -1,35 +1,42 @@
 <?php
- for ($i= 1; $i <= 100 ; $i++) { 
-    if($i % 3 == 0 && $i % 5 == 0){
-        echo "\n". "HACKADEMY";
-        echo " ".$i;
-    }elseif($i % 3 == 0){
-        echo "\n". "PHP";
-        echo " ".$i;
-    }elseif($i % 5 == 0 ){
-        echo "\n". "JAVASCRIPT";
-        echo " ".$i;
-    }
+   for ($i= 1; $i <= 100 ; $i++) { 
 
-    // switch ($i) {
+    // // if/else
+    // if($i % 3 == 0 && $i % 5 == 0){
+    //     echo "\n". "HACKADEMY";
+    //     echo " ".$i;
+    // }elseif($i % 3 == 0){
+    //     echo "\n". "PHP";
+    //     echo " ".$i;
+    // }elseif($i % 5 == 0 ){
+    //     echo "\n". "JAVASCRIPT";
+    //     echo " ".$i;
+    // }else{
+    //     echo "\n".$i;
+    // };
 
-    //     case $i % 3 == 0  && $i % 5 == 0:
-    //         echo "\n". "HACKADEMY";
-    //         echo " ".$i;
-    //         break;
+    switch ($i) {
+        case $i % 3 == 0  && $i % 5 == 0:
+            echo "\n". "HACKADEMY";
+            echo " ".$i;
+            break;
 
-    //     case $i % 3 == 0:
-    //         echo "\n". "PHP";
-    //         echo " ".$i;
-    //         break;
+        case $i % 3 == 0:
+            echo "\n". "PHP";
+            echo " ".$i;
+            break;
 
-    //     case $i % 5 == 0:
-    //         echo "\n". "JAVASCRIPT";
-    //         echo " ".$i;
-    //         break;  
-    // }
+        case $i % 5 == 0:
+            echo "\n". "JAVASCRIPT";
+            echo " ".$i;
+            break;
+            
+        default:
+           echo "\n".$i;
+           break;
+    };
     
- };
+  };
 
 
 
