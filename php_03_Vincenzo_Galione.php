65e583390d4c290138ae6244a79b40908b265c65
VincenzoGalione <165274170+VincenzoGalione@users.noreply.github.com>
2024-05-31 12:27:58 +0200

    terzo esercizio

---
+++ php_03_Vincenzo_Galione.php
@@ -0,0 +1,91 @@
+<?php
+
+$words1 = [
+    'vostro',
+    67,
+    'essere',
+    'colle',
+    'mi',
+    'sempre',
+  [
+    'oscura',
+    'era',
+    89,
+    [
+    'mezzo',
+     'E'
+    ],
+  'ritrovai',
+  'per'
+  ],
+  'diritta'
+  ];
+  
+  $words2 = [
+    'elemento1' => 25.89,
+    'elemento2' => 'possa',
+    'elemento3' => [
+      'Virgilio',
+      'Favore',
+      'favore',
+      ['fortuna']
+    ],
+   'fine' => '!'
+  ];
+
+  $words3 = [
+    'nel',
+    'di',
+    'cammin',
+    'nostra',
+    'vita',
+    'mi',
+    'una',
+    [
+     'selva',
+     ',',
+     'che',
+     [ 
+        'la',
+        'via',
+        'era',
+        'smarrita',
+     ],
+    ],
+  ];
+
+  $results =$words3[0].("\n").$words1[6][3][0].("\n").$words3[1].("\n").$words3[2].("\n").$words3[1].("\n").$words3[3].("\n").$words3[4].("\n").$words3[5].("\n").$words1[6][4].("\n").$words1[6][5].("\n").$words3[6].("\n").$words3[7][0].("\n").$words1[6][0].("\n").$words3[7][1].("\n").$words3[7][2].("\n").$words3[7][3][0].("\n").$words1[7].("\n").$words3[7][3][1].("\n").$words3[7][3][2].("\n").$words3[7][3][3];
+
+//   echo $words3[0];
+//   echo $words1[6][3][0];
+//   echo $words3[1];
+//   echo $words3[2];
+//   echo $words3[1];
+
+//   echo $words3[3];
+//   echo $words3[4];
+//   echo $words3[5];
+
+//   echo $words1[6][4];
+//   echo $words1[6][5];
+//   echo $words3[6];
+//   echo $words3[7][0];
+
+//   echo $words1[6][0];
+//   echo $words3[7][1];
+//   echo $words3[7][2];
+
+//   echo $words3[7][3][0];
+//   echo $words1[7];
+//   echo $words3[7][3][1];
+//   echo $words3[7][3][2];
+//   echo $words3[7][3][3];
+
+  var_dump ($results);
+
+
+
+
+
+
+?>
