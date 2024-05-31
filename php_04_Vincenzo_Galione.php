eb4f3ecf05ea193174c0555848bc486ea5b28c25
VincenzoGalione <165274170+VincenzoGalione@users.noreply.github.com>
2024-05-31 16:32:42 +0200

    quarto esercizio

--- php_04_Vincenzo_Galione.php
+++ php_04_Vincenzo_Galione.php
@@ -1,10 +1,10 @@
 <?php
 
-$users =[
+$users = [
     ['name' => 'Luca', 'surname'=>'Rossi', 'gender'=> 'M'],
     ['name' => 'Eva', 'surname'=>'Baldi', 'gender'=> 'F'],
     ['name' => 'Jessica', 'surname'=>'Ortega', 'gender'=> 'NB'],
-    'Buongiorno',
+    
 
 ];
 
@@ -17,21 +17,16 @@ $users =[
 
 
 
-// foreach ($users as $user) {
-//     if ($user['gender']=='M'){
-//         echo "Buongiorno Sig." . $user['name'] . " " . $user['surname'] ."\n";
-//     }elseif ($user['gender']=='F') {
-//         echo "Buongiorno Sig.ra" . " "  . $user['name'] . " " . $user['surname'] . "\n";
-//     }else{ 
-//         echo "Buongiorno". " " . $user['name'] . " " . $user['surname'] . "\n";
-//     }
-// }
-
-foreach ($users as $user){
-    if($user['gender'] =='M'){
-        echo "Buongiorno Sig." . $user['name'];
+foreach ($users as $user) {
+    if ($user['gender']=='M'){
+        echo "Buongiorno Sig." . $user['name'] . " " . $user['surname'] ."\n";
+    }elseif ($user['gender']=='F') {
+        echo "Buongiorno Sig.ra" . " "  . $user['name'] . " " . $user['surname'] . "\n";
+    }else{ 
+        echo "Buongiorno". " " . $user['name'] . " " . $user['surname'] . "\n";
     }
 }
+
 
 
 
