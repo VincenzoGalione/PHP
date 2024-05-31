3a323bb3efe49598b1bc25f196c6a664d366a52c
VincenzoGalione <165274170+VincenzoGalione@users.noreply.github.com>
2024-05-31 10:37:01 +0200

    primo e secondo esercizio

---
+++ php_01_Vincenzo_Galione.php
@@ -0,0 +1,36 @@
+<?php   
+
+// integer
+$int = 10;
+var_dump($int);
+
+// float
+$float = 5.55;
+var_dump($float);
+
+// string
+$string = 'Ciao a tutti';
+var_dump($string);
+
+// boolean
+$boolean = false;
+var_dump($boolean);
+
+// costante integer
+const NUMBER = 40;
+var_dump(NUMBER);
+
+
+// float
+const VIRGOLA = 10.55;
+var_dump(VIRGOLA);
+
+// string
+const SALUTO = "Buongiorno";
+var_dump(SALUTO);
+
+// boolean
+const FALSO = false;
+var_dump(FALSO);
+
+?>
