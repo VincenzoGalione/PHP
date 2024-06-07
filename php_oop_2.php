840ffa438e9753c448154ac677123c1412adc36f
VincenzoGalione <165274170+VincenzoGalione@users.noreply.github.com>
2024-06-07 10:31:18 +0200

    secondo esercizio php oop

---
+++ php_oop_2.php
@@ -0,0 +1,59 @@
+<?php
+
+class Continent{
+    public $nameContinent;
+
+    public function __construct($continent){
+        $this->nameContinent = $continent;
+    }
+
+}
+
+class Country{
+    public $nameCountry;
+
+    public function __construct($country){
+        $this->nameCountry = $country;
+    }
+}
+
+
+class Region{
+    public $nameRegion;
+
+    public function __construct($region){
+        $this->$nameRegion = $region;
+    }
+}
+
+
+class Province{
+    public $nameProvince;
+
+    public function __construct($province){
+        $this->$nameProvince = $province;
+    }
+}
+
+
+class City{
+public $nameCity;
+
+    public function __construct($city){
+        $this->$nameCity = $city;
+    }
+}
+
+
+class Street{
+    public $nameStreet;
+
+    public function __construct($street){
+        $this->$nameStreet = $street;
+    }
+}
+
+$myLocation = new 
+
+
+?>
