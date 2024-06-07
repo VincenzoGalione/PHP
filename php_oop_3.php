48dce317d11d794f3cbdc1a872ff4a7b76f845ef
VincenzoGalione <165274170+VincenzoGalione@users.noreply.github.com>
2024-06-07 14:31:51 +0200

    terzo esercizio php oop

--- php_oop_3.php
+++ php_oop_3.php
@@ -1,31 +1,94 @@
 <?php
-class Vertebrates{
-    public function __construct($vertebrati){  
+class Vertebrate{
+    public function __construct(){  
+        $this->printVertebrate();
     }
     
-    protected function vertebrato (){
+    protected function printVertebrate (){
         echo"Sono un animale vertebrato\n";
     }
 }
 
-class ColdBlooded extends Vertebrates{
-    public function __construct($vertebrati, $sangueFreddo){
+class Warmblood extends Vertebrate{
+    public function __construct(){
+     parent::__construct();
+     $this->printWarmBlood();
+    }
+
+    protected function printWarmBlood(){
+        echo "Sono un animale a Sangue Caldo\n";
+    }
+}
+
+
+class ColdBlood extends Vertebrate{
+    public function __construct(){
+        parent::__construct();
+        $this->printColdBlood();
     }
     
-    protected function sangueFreddo(){
+    protected function printColdBlood(){
         echo"Sono un animale a Sangue Freddo\n";
     }
 }
 
-class Fish extends ColdBlooded{
-    public function __construct($vertebrati, $sangueFreddo, $fish){
-
+class Mammal extends Warmblood {
+    public function __construct(){
+        parent::__construct();
+        $this->printMammal();
     }
 
-    protected function fish(){
-        echo "Splash!\n";
+    protected function printMammal(){
+        echo "Sono un mammifero\n";
+    }  
+}
+
+class Bird extends Warmblood {
+    public function __construct(){
+        parent::__construct();
+        $this->printBird();
+    }
+
+    protected function printBird(){
+        echo "Sono un uccello\n";
     }
 }
+
+class Fish extends ColdBlood{
+    public function __construct(){
+        parent::__construct();
+        $this->printFish();
+    }
+
+    protected function printFish(){
+        echo"Splash!\n";
+    }
+}
+
+class Reptile extends Coldblood {
+    public function __construct(){
+        parent::__construct();
+        $this->printReptile();
+    }
+
+    protected function printReptile(){
+        echo "Sono un rettile\n";
+    }
+}
+
+class Amphibian extends Coldblood{
+    public function __construct(){
+        parent::__construct();
+        $this->printAmphibian();
+    }
+    
+    protected function printAmphibian(){
+        echo"Sono un anfibio\n";
+    }
+}
+
+$magikarp = new fish();
+
 
 
 ?>
