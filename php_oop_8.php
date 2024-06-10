d5ca65be2fb51e9bf6f166712099e4d3189c0d60
VincenzoGalione <165274170+VincenzoGalione@users.noreply.github.com>
2024-06-10 17:54:18 +0200

    ottavo esercizio php oop

--- php_oop_8.php
+++ php_oop_8.php
@@ -1,39 +1,61 @@
 <?php
 
-    trait Operation{
-        public function sum ($a, $b){
+    trait Calculator{
+        public function sum($a, $b){
             return $a + $b;
         }
         
-        public function sub ($a, $b){
+        public function sub($a, $b){
             return $a - $b;
         }
         
-        public function mul ($a, $b){
+        public function mul($a, $b){
             return $a * $b;
         }
         
-        public function div ($a, $b){
+        public function div($a, $b){
             return $a / $b;
         }
         
-        public function sqr ($a){
-            return sqrt($numero);
+        public function sqr($a){
+            return sqrt($a);
         }   
     }
 
-    class Rettangolo{
-        public $base;
-        public $altezza;
+    class Rectangle{
+        use Calculator;
 
-        public function __construct($a, $b){
-            $this->altezza = $a;
+        public $base;
+        public $height;
+
+        public function __construct($b, $h){
             $this->base = $b;
+            $this->height = $h;
+        }
+
+        public function getSurfaceArea(){
+            return $this->mul($this->base, $this->height);
+        }
+
+        public function getPerimeter(){
+            $sum = $this->sum($this->base, $this->height);
+            $result = $this->mul($sum, 2);
+            return $result;
+        }
+
+        public function getDiagonal(){
+            $baseSquare = $this->base**2;
+            $heightSquare = $this->height**2;
+            $sum = $this->sum($baseSquare, $heightSquare);
+            $result = $this->sqr($sum);
+            return $result;
         }
     }
 
-    class Addizione{
-        use Operation;
-    }
+    $rettangolo = new Rectangle (25, 4);
+    echo $rettangolo->getSurfaceArea() ."\n";
+    echo $rettangolo->getPerimeter() ."\n";
+    echo $rettangolo->getDiagonal() ."\n";
+
 
 ?>
